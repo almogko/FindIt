06155a5a916bb1ca798313ad18a2ae25b81a413b
Tohar Bar <Tohar Bar>
2016-05-13 01:46:33 +0300

    Fuck!

--- app/src/main/java/alto/findit/MainActivity.java
+++ app/src/main/java/alto/findit/MainActivity.java
@@ -7,7 +7,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        // Yes we can!
+		
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
